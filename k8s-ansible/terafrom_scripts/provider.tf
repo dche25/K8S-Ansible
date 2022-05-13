c422a6151f4d45b560c3434070b2aff9f8ab280e
Derek <81638600+dche25@users.noreply.github.com>
2022-05-13 09:09:21 -0400

    Update provider.tf

--- k8s-ansible/terafrom_scripts/provider.tf
+++ k8s-ansible/terafrom_scripts/provider.tf
@@ -8,3 +8,26 @@ terraform {
     }
   }
 }
+
+backend "s3" {
+    bucket = "terraform-eks-bucket1"
+    key    = "prod/terraform.tfstate"
+    region = "us-west-2"
+    # For State Locking
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+resource "aws_dynamodb_table" “tf_lock" {
+  name = "terraform-lock"
+  hash_key = "LockID"
+  read_capacity = 3
+  write_capacity = 3
+  attribute {
+     name = "LockID"
+     type = "S"
+   }
+  tags  {
+    Name = "Terraform Lock Table"
+   }
+ }
